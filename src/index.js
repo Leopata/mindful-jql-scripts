e480fc29ae481f224ebb78cd588749bf644c2ad1
David Miotti <david.miotti@wopata.com>
2017-06-28 15:33:30 +0200

    Added getURL with signature

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import request from 'request-promise';
 import fs from 'fs';
+import crypto from 'crypto';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const endpoint = 'https://mixpanel.com/api/2.0/jql';
-const apiKey = process.env.MIXPANEL_API_SECRET;
-const apiKeyBase64 = new Buffer(`${apiKey}:`).toString('base64');
-const authHeader = { 'Authorization': `Basic ${apiKeyBase64}` };
+const mixpanelApiKey = process.env.MIXPANEL_API_KEY;
+const mixpanelApiSecret = process.env.MIXPANEL_API_SECRET;
+const mixpanelApiToken = process.env.MIXPANEL_API_TOKEN;
+
+const mixpanelBase64Secret = new Buffer(`${mixpanelApiSecret}:`).toString('base64');
+const authHeader = { 'Authorization': `Basic ${mixpanelBase64Secret}` };
 
 // Call Mixpanel API with the JQL script
 const executeJQL = (jql) => {
   const encodedQuery = encodeURIComponent(jql);
-  const uri = `${endpoint}?script=${encodedQuery}`;
-  const opts = { uri: uri, headers: authHeader, json: true };
-  return request(opts);
+  const uri = `https://mixpanel.com/api/2.0/jql?script=${encodedQuery}`;
+  return request({ uri: uri, headers: authHeader, json: true });
 };
 
 // Load the JQL format file
@@ -27,19 +29,54 @@ const readQueryFile = (filename) => {
   });
 };
 
-// Make something useful with the JQL response
-const processJsonResponse = (json) => {
-  const distinctIds = json;
-  console.log(`${distinctIds.length} distinct ids found !`);
-  return distinctIds.slice(1, 2);
+const deleteUsers = (distinctIds) => {
+  let remainingIds = distinctIds;
+
+  const batchSize = 50;
+  const sendBatch = () => {
+    const chunk = remainingIds.splice(0, batchSize);
+
+    console.log(`Deleting ${chunk.length} users...`);
+
+    const data = chunk.map(id => ({ $token: mixpanelApiToken, distinct_id: id, $delete: '' }));
+    const enc = new Buffer(JSON.stringify(data)).toString('base64');
+    const uri = getURL('engage', { verbose: 1, ignore_time: true, ip: 0, data: enc });
+    return request(uri).then(res => {
+      if (remainingIds.length > 0) {
+        return sendBatch();
+      }
+      return res;
+    });
+  };
+
+  return sendBatch();
 };
 
-const deleteUsers = (distinctIds) => {
-  console.log(distinctIds);
+const getURL = (endpoint, opts) => {
+  const expirationTime = 10; // expressed in mins
+  opts.api_key = mixpanelApiKey;
+  opts.expire = Math.round(Date.now() / 1000) + 60 * expirationTime;
+
+  const sortedKeys = Object.keys(opts).sort();
+  const params = [];
+  let concatKeys = "";
+
+  for (let i = 0; i < sortedKeys.length; i++) {
+    const key = sortedKeys[i];
+    params.push(`${key}=${opts[key]}`);
+    concatKeys += params[params.length - 1];
+  }
+
+  let sig = crypto
+    .createHash('md5')
+    .update(concatKeys + mixpanelApiSecret)
+    .digest('hex');
+
+  return `https://api.mixpanel.com/${endpoint}/?${params.join('&')}&sig=${sig}`;
 };
 
 readQueryFile('query.js')
   .then(file => executeJQL(file))
-  .then(json => processJsonResponse(json))
-  .then(distinctIds => deleteUsers(distinctIds))
+  .then(ids => deleteUsers(ids))
+  .then(json => console.log(`Finished with ${JSON.stringify(json)}`))
   .catch(error => console.error(error));
