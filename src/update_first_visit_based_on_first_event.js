f1c2b38853eaa7136d1865e081b06f33fb49a290
David Miotti <david.miotti@wopata.com>
2017-08-09 16:35:47 +0200

    [-] Don't try to fix time zone issues

--- src/update_first_visit_based_on_first_event.js
+++ src/update_first_visit_based_on_first_event.js
@@ -3,16 +3,14 @@ import { readQueryFile, executeJQL, batchEngage } from './jql/execute_jql'
 const formatTuples = (tuples) => tuples.map(tuple => ({
   id: tuple.key[0], date: tuple.value
 }))
-// .filter(item => item.id === '7e50883e-30d5-4835-9a7b-43b62e47e918')
+// .filter(item => item.id === 'pierre.weill@gmail.com')
 
 /// Agregate all user infos
 const agregateUserInfos = (usersWithFirstVisit, usersWithFirstEventDate) => {
   return usersWithFirstEventDate.map(userWithFirstEventDate => {
     const distinctId = userWithFirstEventDate.id
     const matched = usersWithFirstVisit.filter(o => o.id === distinctId)
-
     const firstEventDate = new Date(userWithFirstEventDate.date)
-    firstEventDate.setHours(firstEventDate.getHours() - 2)
 
     let firstVisit = null
     if (matched.length > 0) {
